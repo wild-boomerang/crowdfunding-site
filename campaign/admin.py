a397a8cced0fac12f7aa9d3accca772f3d0d8ac9
boom <zhuravsaki@gmail.com>
2021-06-12 21:13:55 +0300

    Delete unnecessary import

--- campaign/admin.py
+++ campaign/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Campaign, CampaignImage, Bonus, CampaignCategory, Photo
+from .models import Campaign, CampaignImage, Bonus, CampaignCategory
 
 
 @admin.register(CampaignCategory)
