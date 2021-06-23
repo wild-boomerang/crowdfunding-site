3ff774f90734dbe4209a1e3b6b01d1d76e4cb0de
boom <zhuravsaki@gmail.com>
2021-06-23 15:27:10 +0300

    Change expiration date picker to more convenient

--- campaign/forms.py
+++ campaign/forms.py
@@ -8,13 +8,11 @@ class CampaignForm(forms.ModelForm):
         model = Campaign
         fields = ('name', 'description', 'category', 'youtube_id', 'goal', 'expiration_date', 'tags', )
         widgets = {
-            # 'expiration_date': forms.DateInput(attrs={
-            #     'class': 'form-control',
-            #     'type': 'date',
-            # })
-            'expiration_date': forms.SelectDateWidget()
+            'expiration_date': forms.DateInput(attrs={
+                # 'class': 'form-control',
+                'type': 'date',
+            })
         }
-    # expiration_date = forms.DateField(widget=forms.SelectDateWidget)
 
 
 class CommentForm(forms.ModelForm):
